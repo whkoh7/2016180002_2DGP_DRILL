dc0597165baec32bcc175d43ba1eded8fb4cb99c
WooHyuk Kho(Laptop) <whkoh7@naver.com>
2019-10-02 19:00:27 +0900

    Create #Drill_04.py
    2016180002 고우혁
    https://www.youtube.com/watch?v=UQbX-XPvqOc&feature=youtu.be

---
+++ DRILL_04/#Drill_04.py
@@ -0,0 +1,35 @@
+from pico2d import *
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+direction = 1
+x = 0
+frame = 0
+
+while True:
+    if direction == 1:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        get_events()
+        if x == 800:
+            direction = 0
+    elif direction == 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        get_events()
+        if x == 0:
+            direction = 1
+# 여기를 채우세요.
+
+
+close_canvas()
