def51fbe067875ee1e6523b35041ff9cd4a6ed7b
WooHyuk Kho(Laptop) <whkoh7@naver.com>
2019-10-07 19:09:25 +0900

    2

--- Lecture07_Linear_Movement/move_character_with_mouse.py
+++ Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -11,15 +11,13 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             draw = False
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
-            running = True
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            running=True
         elif event.type == SDL_MOUSEMOTION:
             M_x, M_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             draw = False
     pass
-
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
@@ -31,13 +29,19 @@ hide_cursor()
 C_x, C_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 M_x, M_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-
+running=False
 draw = True
 while draw:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, C_x, C_y)
-    cursor.clip_draw(0, 0, 50, 52, M_x, M_y)
+    cursor.clip_draw(0, 0, 50, 52, M_x+25, M_y-25)
+    if(running==True):
+        for i in range(0, 100 + 1, 2):
+            t = i / 100
+            C_x = (1 - t) * C_x + t * M_x
+            C_y = (1 - t) * C_y + t * M_y
+        running=False
     update_canvas()
     frame = (frame + 1) % 8
     handle_events()
